95c106ba4d440cdfb6e12e430c6708af4dfe9ae2
rwa <rwakulszowa1@gmail.com>
2023-06-13 13:17:05 +0200

    feat: simple pagination

---
+++ _data/datasets.js
@@ -0,0 +1,8 @@
+// TODO:
+// - https://ec.europa.eu//eurostat/databrowser-backend/api/public/navtree/en/getFullTree?showProduct=all
+// - https://ec.europa.eu//eurostat/databrowser-backend/api/public/navtree/en/getProductPositionForCategory/general/ei_bp?notFullId=true&showProduct=all
+module.exports = [
+  "general.noneu.noneu_h.cpc.cpc_sc",
+  "general.noneu.noneu_h.cpc.fafa",
+  "some.fake.datum",
+];
