88398ec34bb72503485a0d7fc76907a93562cc61
rwa <rwakulszowa1@gmail.com>
2023-07-08 22:14:16 +0200

    fix: cjs vs 11ty

--- site/.eleventy.cjs
+++ site/.eleventy.cjs
@@ -1,9 +1,4 @@
 module.exports = function (eleventyConfig) {
-  // Map an array by picking `key` of each element.
-  eleventyConfig.addFilter("get", function (value, key) {
-    return value.map((v) => v[key]);
-  });
-
   // Passthrough copy CSS.
   // https://www.11ty.dev/docs/assets/
   eleventyConfig.addPassthroughCopy("bundle.css");
