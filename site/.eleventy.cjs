cf20e742ea7057bd93897dd7c9617fb67181939b
rwa <rwakulszowa1@gmail.com>
2023-07-17 22:25:37 +0200

    fix: css bundle

--- site/.eleventy.cjs
+++ site/.eleventy.cjs
@@ -1,5 +1,1 @@
-module.exports = function (eleventyConfig) {
-  // Passthrough copy CSS.
-  // https://www.11ty.dev/docs/assets/
-  eleventyConfig.addPassthroughCopy("bundle.css");
-};
+module.exports = function (eleventyConfig) {};
