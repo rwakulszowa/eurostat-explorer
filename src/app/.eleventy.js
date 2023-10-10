ad010408421f67f72f1795e534c808177bbc92b3
rwakulszowa <rwakulszowa1@gmail.com>
2023-10-11 01:28:58 +0200

    refactor: unify carousel items

--- src/app/.eleventy.js
+++ src/app/.eleventy.js
@@ -7,6 +7,24 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(lucideIcons, { "stroke-width": 1.5 });
 
+  /**
+   * Prepend a dimensions summary to a list of dimensions and trim redundant data.
+   *
+   * Tightly coupled with the _datasets_ source and a dataset card component.
+   * Not to be used anywhere else.
+   */
+  eleventyConfig.addFilter("datasetDimensions", function (dims) {
+    const dimensionsList = {
+      label: "Dimensions",
+      items: dims.map((d) => d.description),
+    };
+    const categoriesPerDimension = dims.map((d) => ({
+      label: d.description,
+      items: d.positions.slice(0, 5).map((p) => p.description),
+    }));
+    return [dimensionsList, ...categoriesPerDimension];
+  });
+
   eleventyConfig.on("eleventy.before", async () => {
     const app = esbuild.build({
       entryPoints: ["app.ts"],
